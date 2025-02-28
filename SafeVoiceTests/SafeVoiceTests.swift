a90aec96f2320a81996d55f5c8685ae499db6656
elliottcgriffin <elliott.griffin@foyer.ai>
2025-02-28 11:56:15 -0500

    Initial Commit

---
+++ SafeVoiceTests/SafeVoiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  SafeVoiceTests.swift
+//  SafeVoiceTests
+//
+//  Created by Elliott Griffin on 2/28/25.
+//
+
+import Testing
+@testable import SafeVoice
+
+struct SafeVoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
